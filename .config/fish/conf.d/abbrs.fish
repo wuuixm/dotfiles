0564db377737a5e14c1243eed62f3ffa0ce6efb7
wuuixm <wuuixm@outlook.com>
2026-02-19 16:20:24 +0800

    add a two-color waybar solution

--- .config/fish/conf.d/abbrs.fish
+++ .config/fish/conf.d/abbrs.fish
@@ -35,3 +35,15 @@ abbr -a -- zed zeditor
 abbr -a -- nrs niri-session
 abbr -a -- cube 'env _JAVA_AWT_WM_NONREPARENTING=1 GDK_BACKEND=x11 stm32cubemx >/dev/null 2>&1 &'
 abbr -a -- vim nvim
+abbr -a -- js 'copilot -p' # imported from a universal variable, see `help cmds/abbr`
+abbr -a -- bc 'cp dd df du ex hd id ip ln ls mv nl od pr ps rm sg sh ss su tr ul vi wc xz copilot -p' # imported from a universal variable, see `help cmds/abbr`
+abbr -a -- db 'copilot -p' # imported from a universal variable, see `help cmds/abbr`
+abbr -a -- ai copilot
+abbr -a -- ?? 'copilot -p'
+abbr -a -- pac 'sudo pacman -S'
+abbr -a -- fbak 'abbr >> ~/.config/fish/conf.d/abbrs.fish'
+abbr -a -- zed zeditor
+abbr -a -- nrs niri-session
+abbr -a -- cube 'env _JAVA_AWT_WM_NONREPARENTING=1 GDK_BACKEND=x11 stm32cubemx >/dev/null 2>&1 &'
+abbr -a -- vim nvim
+abbr -a -- pacr 'sudo pacman -Rns'
